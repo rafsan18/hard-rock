6e38e48587adfa29a9895b975e8cd0599783aaae
rafsan <rafsanamin18@gmail.com>
2020-08-16 23:37:32 +0600

    messing around with API

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const searchField = document.querySelector(`[data-search-field]`);
+const searchBtn = document.querySelector(`[data-search-btn]`);
 
+searchBtn.addEventListener("click", function () {
+  const searchFieldValue = searchField.value;
+  fetch(`https://api.lyrics.ovh/suggest/${searchFieldValue}`)
+    .then((result) => result.json())
+    .then((searchResult) => {
+      console.log(searchResult);
+    });
+});
