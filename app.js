fd12988e91e3435dde6b26dab6b14ff46f28c0e3
rafsan <rafsanamin18@gmail.com>
2020-08-17 11:14:23 +0600

    title added

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const searchField = document.querySelector(`[data-search-field]`);
 const searchBtn = document.querySelector(`[data-search-btn]`);
+const title = document.querySelectorAll(".lyrics-name");
 
 searchBtn.addEventListener("click", function () {
   const searchFieldValue = searchField.value;
   fetch(`https://api.lyrics.ovh/suggest/${searchFieldValue}`)
     .then((result) => result.json())
     .then((searchResult) => {
-      console.log(searchResult);
+      console.log(searchResult.data);
+      // let title = document.querySelector(".lyrics-name");
+      // title.innerText = searchResult.data[0].album.title;
+      console.log(title);
+      for (let i = 0; i < title.length; i++) {
+        title[i].innerText = searchResult.data[i].title;
+      }
     });
 });
